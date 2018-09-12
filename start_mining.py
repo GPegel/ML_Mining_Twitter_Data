fa149b968bab5afec6bf79793dd053365f86543b
Gerhard Pegel <gpegel@gmail.com>
2018-09-12 14:27:58 +0200

    added some cool stuff

--- start_mining.py
+++ start_mining.py
@@ -1,4 +1,5 @@
 import json
+
 import tweepy
 from env import *
 
@@ -9,17 +10,19 @@ auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    process_or_store(status._json)
+
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+
+# for status in tweepy.Cursor(api.home_timeline).items(10):
+#     # Process a single status
+#     print(json.dumps(status._json))
 
 # What if we want to have a list of all our followers? There you go:
-for friend in tweepy.Cursor(api.friends).items():
-    process_or_store(friend._json)
+# for friend in tweepy.Cursor(api.friends).items():
+#     print(json.dumps(friend._json))
 
 # And how about a list of all our tweets? Simple:
 for tweet in tweepy.Cursor(api.user_timeline).items():
     process_or_store(tweet._json)
 
-def process_or_store(tweet):
-    print(json.dumps(tweet))
